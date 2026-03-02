e954cde42cf6e44ff4f7ee2671247b28ccaf643c
Shivsharan yadav <shivsharanyadav9516@gmail.com>
2026-03-02 13:16:47 +0530

    taking input from the user

--- First.java
+++ First.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public static void main(String[] args) {
     System.out.println("Hello, World!");
-    System.out.println("Hello, Java2!");
-    System.out.println("Hello, Java3!");
+    // take input and add to a variable
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(a + b);
+   
+    
 }
